b186543b0ff1a99c6ae5d90cd2b607223b27731d
ahmaf1 <ahmaf1@bfh.c>
2025-02-23 01:00:08 +0100

    header and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Header } from "../sections/Header";
-
+import { HeroSection } from "../sections/Hero";
 export default function Home() {
   return (
     <div>
       <Header />
+      <HeroSection />
       <h1>Home page</h1>
     </div>
   );
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,22 @@
 export const Header = () => {
   return (
-    <div>
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">Projects</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+    <div className="fixed top-3 w-full flex justify-center items-center">
+      <nav className="flex gap-4 p-2 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a href="#" className="nav-item">
+          Home
+        </a>
+        <a href="#" className="nav-item">
+          Projects
+        </a>
+        <a href="#" className="nav-item">
+          About
+        </a>
+        <a
+          href="#"
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+        >
+          Contact
+        </a>
       </nav>
     </div>
   );
